bb698f77fcd3b9aac037e95f9952969ce63d059b
Riki Ogasawara <r.bvbsaw22@gmail.com>
2021-04-26 12:55:38 +0900

    オートログイン、ログアウトボタンを実装

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 
+import LogOutButton from '../components/LogOutButton';
 import HomeContents from '../components/HomeContents';
 
-export default function HomeScreen() {
+export default function HomeScreen(props) {
+  const { navigation } = props;
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <LogOutButton />,
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <HomeContents />
